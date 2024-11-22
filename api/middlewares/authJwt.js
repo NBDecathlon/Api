ea949add0fa5a10bd5b80cdb32d03f4c614b19d3
Nassim Bentifraouine <nassim.bentifraouine@decathlon.com>
2024-11-22 10:28:52 +0100

    rateLimiter

--- api/middlewares/authJwt.js
+++ api/middlewares/authJwt.js
@@ -3,30 +3,19 @@ const config = require('../config/key');
 
 const verifyToken = (req, res, next) => {
     const token = req.headers['x-access-token'];
-    if (!token) return res.status(403).send({ message: 'Aucun token fourni.' });
+    if (!token) return res.status(403).send({ message: 'No token provided' });
 
     jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) return res.status(401).send({ message: 'Non autorisé !' });
+        if (err) return res.status(401).send({ message: 'Not Authorized!' });
         req.userId = decoded.id;
-        req.role = decoded.role;
+        req.role = decoded.role || "user";
         next();
     });
 };
 
 const isAdmin = (req, res, next) => {
-    if (req.role === 'Admin') {
-        next();
-    } else {
-        res.status(403).send({ message: 'Accès réservé aux administrateurs.' });
-    }
+    if (req.role !== "admin") return res.status(403).send({ message: 'Refused access' });
+    next();
 };
 
-const isUser = (req, res, next) => {
-    if (req.role === 'User' || req.role === 'Admin') {
-        next();
-    } else {
-        res.status(403).send({ message: 'Accès réservé aux utilisateurs.' });
-    }
-};
-
-module.exports = { verifyToken, isAdmin, isUser };
+module.exports = { verifyToken, isAdmin };
--- api/middlewares/rateLimiter.js
+++ api/middlewares/rateLimiter.js
@@ -1,10 +1,16 @@
 const setRateLimit = require("express-rate-limit");
 
-const rateLimitMiddleware = setRateLimit({
-    windowMs: 60 * 1000,
-    max: 5,
-    message: "You have exceeded your 5 requests per minute limit.",
-    headers: true,
-  });
-  
-  module.exports = rateLimitMiddleware;
+const rateLimitMiddleware = (req, res, next) => {
+    const isAdmin = req.role === "admin";
+
+    const limiter = setRateLimit({
+        windowMs: 1000,
+        max: isAdmin ? 10 : 5,
+        message: "You have exceeded your request limit.",
+        headers: true,
+    });
+
+    limiter(req, res, next);
+};
+
+module.exports = rateLimitMiddleware;
