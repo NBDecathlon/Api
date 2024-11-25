2b411aa72ec9a76030dbfc460e5843fe654bf786
Nassim Bentifraouine <nassim.bentifraouine@decathlon.com>
2024-11-25 16:11:29 +0100

    last commmit

--- api/controller/authcontroller.js
+++ api/controller/authcontroller.js
@@ -2,52 +2,58 @@ const User = require("../models/user");
 var bcrypt = require("bcryptjs");
 const config = require("../config/key");
 var jwt = require("jsonwebtoken");
+const axios = require("axios");
 
-exports.signup = async (req, res) => {
-    const user = new User({
-      username: req.body.username,
-      name: req.body.name,
-      password: bcrypt.hashSync(req.body.password, 8),
-      role: req.body.role || 'User',
+const CLIENT_ID = "Ov23lieqe9tnNqMecUxI";
+const CLIENT_SECRET = "9d4e71dc1eeb6c9819672f71a3dc1769d958fe95";
+const GITHUB_URL = "https://github.com/login/oauth/access_token";
+
+exports.oauth2Redirect = async (req, res) => {
+  try {
+    const response = await axios({
+      method: "POST",
+      url: `${GITHUB_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+      headers: {
+        Accept: "application/json",
+      },
     });
-    try {
-      await user.save();
-      res.send({ message: "Utilisateur enregistré avec succès !" });
-    } catch (err) {
-      console.log(err);
-      res.status(500).send("Erreur lors de la création du compte");
-    }
-};
-
-exports.signin = async (req, res) => {
-    const user = await User.findOne({ username: req.body.username });
+    const userData = await axios({
+      method: "GET",
+      url: `https://api.github.com/user`,
+      headers: {
+        Authorization: "token " + response.data.access_token,
+      },
+    });
+    console.log(userData);
+    const { login, name } = userData.data;
+    let user = await User.findOne({ login }).select("-password");
     if (!user) {
-      return res.status(404).send({ message: "Utilisateur non trouvé !" });
-    }
-
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-    if (!passwordIsValid) {
-      return res.status(401).send({
-        accessToken: null,
-        message: "Mot de passe invalide !",
+       user = new User({
+        username: login,
+        name: name,
       });
+      try {
+        await user.save();
+      } catch (err) {
+        console.log(err);
+        res.status(500).send("Error while creating the account");
+      }
     }
-
     const token = jwt.sign(
-      { id: user.id, username: user.username, role: user.role },
+      { id: user.id, username: user.username },
       config.secret,
       {
-        algorithm: 'HS256',
+        algorithm: "HS256",
         allowInsecureKeySizes: true,
-        expiresIn: 86400,
+        expiresIn: 86400, // 24 hours
       }
     );
-
-    res.status(200).send({
-      id: user._id,
-      username: user.username,
-      name: user.name,
-      role: user.role,
-      accessToken: token,
-    });
+    
+    res.redirect(
+      `http://localhost:8080/todo.html?access_token=${token}`
+    );
+  } catch (error) {
+    console.error("Error during GitHub authentication:", error);
+    res.status(500).send("Error during GitHub authentication");
+  }
 };
